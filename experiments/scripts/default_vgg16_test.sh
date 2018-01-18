f4ecd21a6234e678ce107e7468c66644733508d6
Mengmeng Xu <xum@kw60249.cbrc.kaust.edu.sa>
2018-01-18 09:40:37 +0300

    add test batch

---
+++ experiments/scripts/default_vgg16_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export PYTHONUNBUFFERED="True"
+
+LOG="experiments/logs/default_vgg16.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+
+time ./tools/test_net.py --gpu $1 \
+  --def models/VGG16/test.prototxt \
+  --net output/default/voc_2007_trainval9/vgg16_fast_rcnn_iter_40000.caffemodel \
+  --imdb voc_2007_test
+
